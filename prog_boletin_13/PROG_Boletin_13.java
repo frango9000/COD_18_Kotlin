73728c0f4c6064853d28ff4df1c285ff6c92736d
Francisco Sanchez <fsancheztemprano@danielcastelao.org>
2018-12-04 12:17:41 +0100

    * instancia un obxecto de cada clase      * instancia un xogador de tipo Seleccion      * chama ao método concentrarse a través de cada obxecto.      * Invoca ao método viaxar a través de cada obxecto.      * ¿ Podes invocar a un método da superclase con un obxecto dunha subclase ?      * ¿ Podes invocar a un método da subclase con un obxecto do tipo da superclase ?

--- prog_boletin_13/PROG_Boletin_13.java
+++ prog_boletin_13/PROG_Boletin_13.java
@@ -15,7 +15,19 @@ public class PROG_Boletin_13 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Entrenador entrenador = new Entrenador("IDF0008",1, "Nombre", "Apellido", 30);
+        Jugador jugador = new Jugador(1, "Portero", 2,"Nombre2", "Apellido2",19);
+        Masajista masajista = new Masajista("Titulo", 10, 3, "Nombre Masajista", "Apellido Masajista", 45);
+        Seleccion jugador2 = new Jugador(1, "Portero", 2,"Nombre2", "Apellido2",19);
+        entrenador.concentrarse();
+        jugador.concentrarse();
+        masajista.concentrarse();
+        jugador2.concentrarse();
+        
+        entrenador.viajar();
+        jugador.viajar();
+        masajista.viajar();
+        jugador2.viajar();
     }
     
 }
